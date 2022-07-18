b0a81182dc533f1d4b02bc4e0a4fa23c8ae7dc18
Elson Otake <elsonotake@yahoo.com.br>
2022-07-18 18:33:56 -0300

    Update action function, slice and reducer

--- src/redux/missions/Missions.js
+++ src/redux/missions/Missions.js
@@ -1,5 +1,54 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import { axiosGet } from '../api';
+
+const FETCH = 'space-travelers/missions/FETCH';
+const urlMissions = 'https://api.spacexdata.com/v3/missions';
+const initialState = {
+  loading: false,
+};
+
 const missionsReducer = (state = [], action = {}) => {
-  return state;
-}
+  switch (action.type) {
+    case FETCH:
+      return action.payload;
+    default:
+      return state;
+  }
+};
+
+export const fetchMissions = createAsyncThunk(
+  FETCH,
+  async (_, { dispatch }) => {
+    const res = await axiosGet(urlMissions)
+      .then(
+        (data) => dispatch({
+          type: FETCH,
+          payload: data,
+        }),
+      );
+    return res;
+  },
+);
+
+export const missionsSlice = createSlice({
+  name: 'missionsSlice',
+  initialState,
+  extraReducers: {
+    [fetchMissions.pending]: (state) => ({
+      ...state,
+      loading: true,
+    }),
+    [fetchMissions.fulfilled]: (state) => ({
+      ...state,
+      loading: false,
+    }),
+    [fetchMissions.rejected]: (state) => ({
+      ...state,
+      loading: false,
+    }),
+  },
+});
+
+export const missionsSliceReducer = missionsSlice.reducer;
 
 export default missionsReducer;
