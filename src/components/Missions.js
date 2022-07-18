606fe1d5f7df90802de2d147771b2f6ff88ba323
Elson Otake <elsonotake@yahoo.com.br>
2022-07-18 18:33:06 -0300

    Update useEffect, useSelector, and useDispatch included

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,7 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchMissions } from '../redux/missions/Missions';
 
-const Missions = () => (
-  <h1>Missions</h1>
+const Missions = () => {
+  const missionsData = useSelector((state) => state.missionsReducer);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchMissions);
+  }, []);
+
+  return (
+    <ul>
+      {missionsData.map((data) => (
+        <li key={data.mission_id}>
+          {data.mission_name}
+        </li>
+      ))}
+    </ul>
 );
+};
 
 export default Missions;
