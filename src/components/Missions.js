4446ab4d4e811778680b7f8b031ef037745336ab
Elson Otake <elsonotake@yahoo.com.br>
2022-07-18 20:26:44 -0300

    Update useEffect to dispatch just once

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,23 +1,53 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { fetchMissions } from '../redux/missions/Missions';
+import { Container, Row, Col, Table } from 'react-bootstrap';
 
 const Missions = () => {
   const missionsData = useSelector((state) => state.missionsReducer);
   const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(fetchMissions());
-  }, []);
+    (missionsData.length === 0) && dispatch(fetchMissions());
+  }, [missionsData.length]);
 
   return (
-    <ul>
-      {missionsData.map((data) => (
+    <Container>
+      {/* {missionsData.map((data) => (
         <li key={data.mission_id}>
           {data.mission_name}
         </li>
-      ))}
-    </ul>
+      ))} */}
+      <Row>
+        <Col>
+          <Table striped bordered hover>
+            <thead>
+              <tr>
+                <th>Mission</th>
+                <th>Description</th>
+                <th>Status</th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              {
+                missionsData.length > 0 &&
+                missionsData.map((data) => {
+                  return (
+                    <tr key={data.mission_id}>
+                      <td>{data.mission_name}</td>
+                      <td>{data.description}</td>
+                      <td></td>
+                      <td></td>
+                    </tr>
+                  )
+                })
+              }
+            </tbody>
+          </Table>
+        </Col>
+      </Row>
+    </Container>
 );
 };
 
