a11792150ce27def150a1362a82087d7d0faac30
Elson Otake <elsonotake@yahoo.com.br>
2022-07-18 17:41:11 -0300

    Update route to Missions component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
+import Missions from './components/Missions';
 import './App.css';
 
 const App = () => (
   <React.StrictMode>
     <Router>
       <Header />
-      {/* <Routes>
-        <Route path="/" element={<Rockets />} />
-        <Route path="categories" element={<Missions />} />
-      </Routes> */}
+      <Routes>
+        {/* <Route path="/" element={<Rockets />} /> */}
+        <Route path="/missions" element={<Missions />} />
+      </Routes>
     </Router>
   </React.StrictMode>
 );
