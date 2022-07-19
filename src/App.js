f316a713ecf183a4134ceebdb2d7f68f9831e2d9
Elson Otake <elsonotake@yahoo.com.br>
2022-07-19 18:10:38 -0300

    Update route to MyProfile

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Missions from './components/Missions';
 import './App.css';
+import MyProfile from './components/MyProfile';
 
 const App = () => (
   <React.StrictMode>
@@ -11,6 +12,7 @@ const App = () => (
       <Routes>
         {/* <Route path="/" element={<Rockets />} /> */}
         <Route path="/missions" element={<Missions />} />
+        <Route path="/my_profile" element={<MyProfile />} />
       </Routes>
     </Router>
   </React.StrictMode>
