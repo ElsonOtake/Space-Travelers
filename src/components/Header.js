176634d30380d7644289310c660ff3b7c96fe74a
Elson Otake <elsonotake@yahoo.com.br>
2022-07-19 13:00:19 -0300

    Update import

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
-import Image from 'react-bootstrap/Image';
 
 const Header = () => (
   <Navbar variant="primary">
