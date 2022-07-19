b3356c3521232ac1629d1ac89c6645b8a7b00e4c
Elson Otake <elsonotake@yahoo.com.br>
2022-07-19 17:22:43 -0300

    Update React conditional rendering syntax

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -15,23 +15,6 @@ const Missions = () => {
 
   const handleClick = (e) => {
     const id = e.target.classList[0].substr(4);
-    const button = document.querySelector(`.btn-${id}`);
-    const badge = document.querySelector(`.bdg-${id}`);
-    if (button.firstChild.innerText === "Join Mission") {
-      button.firstChild.innerText = "Leave Mission";
-      button.classList.remove("btn-outline-secondary");
-      button.classList.add("btn-outline-danger");
-      badge.firstChild.innerText = "Active Member";
-      badge.classList.remove("bg-secondary");
-      badge.classList.add("bg-info");
-    } else {
-      button.firstChild.innerText = "Join Mission";
-      button.classList.remove("btn-outline-danger");
-      button.classList.add("btn-outline-secondary");
-      badge.firstChild.innerText = "NOT A MEMBER";
-      badge.classList.remove("bg-info");
-      badge.classList.add("bg-secondary");
-    }
     dispatch(joinMission(id));
   }
 
@@ -73,22 +56,42 @@ const Missions = () => {
                       </td>
                       <td className="align-middle">
                         <div className="d-flex justify-content-center flex-wrap">
-                          <Badge bg="secondary" className={`bdg-${mission.id}`}>
-                            <small>NOT A MEMBER</small>
-                          </Badge>
+                          {!mission.reserved && ( 
+                            <Badge bg="secondary">
+                              <small>NOT A MEMBER</small>
+                            </Badge>
+                          )}
+                          {mission.reserved && ( 
+                            <Badge bg="info">
+                              <small>Active Member</small>
+                            </Badge>
+                          )}
                         </div>
                       </td>
                       <td className="align-middle">
-                        <div className="d-flex justify-content-center">
-                          <Button variant="outline-secondary" className={`btn-${mission.id}`}
-                            onClick={
-                              (e) => {
-                                handleClick(e)
+                        <div className="d-flex justify-content-center flex-wrap">
+                          {!mission.reserved && ( 
+                            <Button variant="outline-secondary" className={`btn-${mission.id}`}
+                              onClick={
+                                (e) => {
+                                  handleClick(e)
+                                }
                               }
-                            }
-                          >
-                            <small className={`sml-${mission.id}`}>Join Mission</small>
-                          </Button>
+                            >
+                              <small className={`sml-${mission.id}`}>Join Mission</small>
+                            </Button>
+                          )}
+                          {mission.reserved && ( 
+                            <Button variant="outline-danger" className={`btn-${mission.id}`}
+                              onClick={
+                                (e) => {
+                                  handleClick(e)
+                                }
+                              }
+                            >
+                              <small className={`sml-${mission.id}`}>Leave Mission</small>
+                            </Button>
+                          )}
                         </div>
                       </td>
                     </tr>
