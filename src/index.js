75a996529e3f08a1ca59a6be34a7659fdbd90839
Elson Otake <elsonotake@yahoo.com.br>
2022-07-19 20:25:58 -0300

    Delete index.css

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import store from './redux/configureStore';
-import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
