b7076df3c0e2835e7e9878bf6bcd43c84899dfad
Elson Otake <elsonotake@yahoo.com.br>
2022-07-20 15:40:52 -0300

    Add tests for MyProfile component

---
+++ src/__tests__/MyProfile.test.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import MyProfile from '../components/MyProfile';
+import { Provider } from 'react-redux';
+import store from '../redux/configureStore';
+
+describe('Tests for the MyProfile component', () => {
+  render(
+    <Provider store={store}>
+      <MyProfile />
+    </Provider>,
+  );
+  test('Check for the following test on screen', () => {
+
+    expect(screen.getByText('My Missions')).toBeInTheDocument();
+    expect(screen.getByText('My Rockets')).toBeInTheDocument();
+
+});
+  test('Check for the snapshot', () => {
+    expect(screen.debug()).toMatchSnapshot();
+  });
+});
